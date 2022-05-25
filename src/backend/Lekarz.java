20d6279f5d754fb183af0af5e93714044e0c48d4
jkrotoszynska <justyna.krotoszynska@gmail.com>
2022-05-25 13:23:34 +0200

    backend

--- src/backend/Lekarz.java
+++ src/backend/Lekarz.java
@@ -1,5 +1,58 @@
 package backend;
 
 public class Lekarz {
+    private int idLekarza;
+    private String imie, nazwisko; 
+    private String specjalizacja, nazwaOddzialu;
+
+    public Lekarz(int idLekarza, String imie, String nazwisko, String specjalizacja, String nazwaOddzialu) {
+        this.idLekarza = idLekarza;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.specjalizacja = specjalizacja;
+        this.nazwaOddzialu = nazwaOddzialu;
+    }
+
+    public int getIdLekarza() {
+        return idLekarza;
+    }
+
+    public void setIdLekarza(int idLekarza) {
+        this.idLekarza = idLekarza;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getSpecjalizacja() {
+        return specjalizacja;
+    }
+
+    public void setSpecjalizacja(String specjalizacja) {
+        this.specjalizacja = specjalizacja;
+    }
+
+    public String getNazwaOddzialu() {
+        return nazwaOddzialu;
+    }
+
+    public void setNazwaOddzialu(String nazwaOddzialu) {
+        this.nazwaOddzialu = nazwaOddzialu;
+    }
+    
+    
     
 }
