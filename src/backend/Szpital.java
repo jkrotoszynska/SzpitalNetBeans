e8385a0533d5aa1a79a0ca8428136f9d23d6e899
Kajetan Warmbier <kajetan.warmbier@gmail.com>
2022-05-25 16:30:18 +0200

    JPA CONFIG WITH JAR

--- src/backend/Szpital.java
+++ src/backend/Szpital.java
@@ -1,13 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package backend;
 
-public class Szpital {
-    
-    private String nazwaOddzialu; 
-    private int numerOddzialu;
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-    public Szpital(String nazwaOddzialu, int numerOddzialu) {
+/**
+ *
+ * @author kajet
+ */
+@Entity
+@Table(name = "Szpital")
+@NamedQueries({
+    @NamedQuery(name = "Szpital.findAll", query = "SELECT s FROM Szpital s"),
+    @NamedQuery(name = "Szpital.findByNazwaOddzialu", query = "SELECT s FROM Szpital s WHERE s.nazwaOddzialu = :nazwaOddzialu"),
+    @NamedQuery(name = "Szpital.findByNumerOddzialu", query = "SELECT s FROM Szpital s WHERE s.numerOddzialu = :numerOddzialu")})
+public class Szpital implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "nazwaOddzialu")
+    private String nazwaOddzialu;
+    @Column(name = "numerOddzialu")
+    private Integer numerOddzialu;
+    @OneToMany(mappedBy = "nazwaOddzialu")
+    private Collection<HistoriaLeczen> historiaLeczenCollection;
+    @OneToMany(mappedBy = "nazwaOddzialu")
+    private Collection<Lekarz> lekarzCollection;
+
+    public Szpital() {
+    }
+
+    public Szpital(String nazwaOddzialu) {
         this.nazwaOddzialu = nazwaOddzialu;
-        this.numerOddzialu = numerOddzialu;
     }
 
     public String getNazwaOddzialu() {
@@ -18,13 +54,53 @@ public class Szpital {
         this.nazwaOddzialu = nazwaOddzialu;
     }
 
-    public int getNumerOddzialu() {
+    public Integer getNumerOddzialu() {
         return numerOddzialu;
     }
 
-    public void setNumerOddzialu(int numerOddzialu) {
+    public void setNumerOddzialu(Integer numerOddzialu) {
         this.numerOddzialu = numerOddzialu;
     }
-    
+
+    public Collection<HistoriaLeczen> getHistoriaLeczenCollection() {
+        return historiaLeczenCollection;
+    }
+
+    public void setHistoriaLeczenCollection(Collection<HistoriaLeczen> historiaLeczenCollection) {
+        this.historiaLeczenCollection = historiaLeczenCollection;
+    }
+
+    public Collection<Lekarz> getLekarzCollection() {
+        return lekarzCollection;
+    }
+
+    public void setLekarzCollection(Collection<Lekarz> lekarzCollection) {
+        this.lekarzCollection = lekarzCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (nazwaOddzialu != null ? nazwaOddzialu.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Szpital)) {
+            return false;
+        }
+        Szpital other = (Szpital) object;
+        if ((this.nazwaOddzialu == null && other.nazwaOddzialu != null) || (this.nazwaOddzialu != null && !this.nazwaOddzialu.equals(other.nazwaOddzialu))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "backend.Szpital[ nazwaOddzialu=" + nazwaOddzialu + " ]";
+    }
     
 }
