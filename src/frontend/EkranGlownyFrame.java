1e8a1b8a188cb1e48d928178b99ed92b3f417a18
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-05-25 17:42:38 +0200

    buttons

--- src/frontend/EkranGlownyFrame.java
+++ src/frontend/EkranGlownyFrame.java
@@ -1,6 +1,4 @@
-
 package frontend;
-
 
 public class EkranGlownyFrame extends javax.swing.JFrame {
 
@@ -242,62 +240,31 @@ public class EkranGlownyFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void pacjentButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pacjentButtonActionPerformed
-        // TODO add your handling code here:
         pacjenci.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_pacjentButtonActionPerformed
 
     private void historiaButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_historiaButtonActionPerformed
-        // TODO add your handling code here:
         historia.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_historiaButtonActionPerformed
 
     private void wyszukiwarkaButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_wyszukiwarkaButtonActionPerformed
-        // TODO add your handling code here:
         wyszukiwarka.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_wyszukiwarkaButtonActionPerformed
 
     private void szczegolyButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_szczegolyButtonActionPerformed
-        // TODO add your handling code here:
         szczegoly.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_szczegolyButtonActionPerformed
 
     private void wylogujButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_wylogujButtonActionPerformed
-        // TODO add your handling code here:
         this.setVisible(false);
     }//GEN-LAST:event_wylogujButtonActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(EkranGlownyFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(EkranGlownyFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(EkranGlownyFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(EkranGlownyFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new EkranGlownyFrame().setVisible(true);
